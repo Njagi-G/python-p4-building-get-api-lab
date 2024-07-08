13ea3d1c29bf6c3f669f5ddb8dcd875e8aa1ead1
Njagi Githaiga <njagigithaiga@gmail.com>
2024-07-08 18:37:54 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,41 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    response = make_response(bakeries, 200)
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(bakery_dict, 200)
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    bg_p = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    bg_p_dict = [b.to_dict() for b in bg_p]
+
+    response = make_response(bg_p_dict, 200)
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    me_bg = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+
+    me_bg_dict = me_bg.to_dict()
+
+    response = make_response(me_bg_dict, 200)
+    
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
